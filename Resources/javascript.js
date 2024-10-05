a5409873bbc664411fb44cdf081f720833cfa3dc
Damo89 <damien.thomas89@hotmail.co.uk>
2024-10-05 21:40:31 +0100

    Creation of Quotes Array

---
+++ Resources/javascript.js
@@ -0,0 +1,53 @@
+const generateQuote = function () {
+    const quotes = [
+        {
+            quote: "If I cannot do great things, I can do small things in a great way.",
+            author: "Martin Luther King, Jr."
+        },
+
+        {
+            quote: "Be the change that you wish to see in the world.",
+            author: "Mahatma Ghandi"
+        },
+
+        {
+            quote: "Opportunity is missed by most people because it is dressed in overalls and looks like work.",
+            author: "Thomas Edison"
+        },
+
+        {
+            quote: "The bad news is time flies. The good news is you're the pilot.",
+            author: "Michael Altshuler"
+        },
+
+        {
+            quote: "I can't change the direction of the wind, but I can adjust my sails to always reach my destination.",
+            author: "Jimmy Dean"
+        },
+
+        {
+            quote: "It is often the small steps, not the giant leaps, that bring about the most lasting change.",
+            author: "Queen Elizabeth II"
+        },
+
+        {
+            quote: "If my mind can conceive it, if my heart can believe it, then I can achieve it.",
+            author: "Muhammed Ali"
+        },
+
+        {
+            quote: "Believe you can and you're halfway there.",
+            author: "Theodore Roosevelt"
+        },
+
+        {
+            quote: "The people who are crazy enough to think they can change the world are the ones who do.",
+            author: "Steve Jobs"
+        },
+
+        {
+            quote: "If I cannot do great things, I can do small things in a great way.",
+            author: "Norman Vincent Peale"
+        }
+    ]
+}
